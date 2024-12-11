de728fa94e5b1c42bfc638c69023a907ad753eb9
jahnavi190 <147274117+jahnavi190@users.noreply.github.com>
2024-12-11 22:48:08 +0530

    Create ErpManagementApplicationTests.java

---
+++ src/test/java/com/example/ERP_MANAGEMENT/ErpManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ERP_MANAGEMENT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

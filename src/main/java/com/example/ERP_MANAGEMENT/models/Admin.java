b0268a8c22d826c0a153cf07af98cf04aca0d6e8
jahnavi190 <147274117+jahnavi190@users.noreply.github.com>
2024-12-11 22:40:08 +0530

    Create Admin.java

---
+++ src/main/java/com/example/ERP_MANAGEMENT/models/Admin.java
@@ -0,0 +1,67 @@
+package com.example.ERP_MANAGEMENT.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String adminname;
+    private String mobileNumber;
+    private String mail;
+    private String address;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getAdminname() {
+        return adminname;
+    }
+    public void setAdminname(String adminname) {
+        this.adminname = adminname;
+    }
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Admin() {}
+}
